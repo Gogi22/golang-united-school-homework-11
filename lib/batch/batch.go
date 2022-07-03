5235dce4104b1707e0a5d6d7ef9c6a95f6546907
Gogi <gogi.mamutchadze@lb.ge>
2022-07-03 14:41:37 +0400

    finished problem with errgroup worket pool

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,9 @@
 package batch
 
 import (
+	"context"
+	"golang.org/x/sync/errgroup"
+	"sync"
 	"time"
 )
 
@@ -14,5 +17,51 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	//return getBatchWorkerPool(n, pool)
+	return getBatchErrGroup(n, pool)
+}
+
+func getBatchErrGroup(n int64, pool int64) (res []user) {
+	errG, _ := errgroup.WithContext(context.Background())
+	errG.SetLimit(int(pool))
+
+	var mu sync.Mutex
+
+	for i := int64(0); i < n; i++ {
+		id := i
+		errG.Go(func() error {
+			ans := getOne(id)
+			mu.Lock()
+			defer mu.Unlock()
+			res = append(res, ans)
+			return nil
+		})
+	}
+
+	_ = errG.Wait()
+	return
+}
+
+func getBatchWorkerPool(n int64, pool int64) (res []user) {
+	pendingJobs := make(chan int64, n/2)
+	finishedJobs := make(chan user, n/2)
+
+	for i := int64(0); i < pool; i++ {
+		go func() {
+			for j := range pendingJobs {
+				finishedJobs <- getOne(j)
+			}
+		}()
+	}
+
+	for i := int64(0); i < n; i++ {
+		pendingJobs <- i
+	}
+	close(pendingJobs)
+
+	for i := int64(0); i < n; i++ {
+		res = append(res, <-finishedJobs)
+	}
+
+	return
 }
